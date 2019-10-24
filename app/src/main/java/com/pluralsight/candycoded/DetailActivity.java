b27853d83e03ef7b630e0d9573a060cec640e7e4
loicleprogrammeur <ngapmennyamiyannickloic@gmail.com>
2019-10-24 08:00:02 +0100

    commit

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -22,21 +22,6 @@ public class DetailActivity extends AppCompatActivity {
     public static final String HASHTAG_CANDYCODED = " #candycoded";
     String mCandyImageUrl = "";
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        createShareIntent();
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void createShareIntent(){
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.setType("text/plain");
-        String shareString = SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED;
-        shareIntent.putExtra(Intent.EXTRA_TEXT , shareString);
-        startActivity(shareIntent);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -87,4 +72,19 @@ public class DetailActivity extends AppCompatActivity {
     // ***
     // TODO - Task 4 - Share the Current Candy with an Intent
     // ***
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        createShareIntent();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void createShareIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        String shareString = SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED;
+        shareIntent.putExtra(Intent.EXTRA_TEXT , shareString);
+        startActivity(shareIntent);
+    }
 }
